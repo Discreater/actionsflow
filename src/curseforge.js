7802cc2b54fe8df83523086944e6abace11817b0
Discreater <941354277@qq.com>
2021-07-26 09:50:58 +0000

    feat: add release type map

--- src/curseforge.js
+++ src/curseforge.js
@@ -1,5 +1,21 @@
 
 module.exports = async function ({ helpers }) {
     const result = await helpers.axios.get("https://addons-ecs.forgesvc.net/api/v2/addon/242223/files")
-    return result.data;
+    const data = result.data;
+    switch (data.releaseType) {
+        case 1:
+            data.releaseTypeStr = 'Release';
+            break;
+        case 2:
+            data.releaseTypeStr = 'Beta';
+            break;
+        case 3: 
+            data.releaseTypeStr = 'Alpha';
+            break;
+        default:
+            data.releaseTypeStr = `Unknow: ${data.releaseType}`;
+            break;
+    }
+    
+    return data;
 }
