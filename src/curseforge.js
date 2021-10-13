011e9be216590be371958c30993407f9a689edfb
Discreater <Discreater@163.com>
2021-10-13 16:17:03 +0800

    feat(curse): add exception catch

--- src/curseforge.js
+++ src/curseforge.js
@@ -1,24 +1,29 @@
 module.exports = async function ({ helpers }) {
-    const result = await helpers.axios.get("https://addons-ecs.forgesvc.net/api/v2/addon/242223/files")
-    const datas = result.data;
-    for (data of datas) {
-        switch (data.releaseType) {
-            case 1:
-                data.releaseTypeStr = 'Release';
-                break;
-            case 2:
-                data.releaseTypeStr = 'Beta';
-                break;
-            case 3:
-                data.releaseTypeStr = 'Alpha';
-                break;
-            default:
-                data.releaseTypeStr = `Unknow: ${data.releaseType}`;
-                break;
+    try {
+        const result = await helpers.axios.get("https://addons-ecs.forgesvc.net/api/v2/addon/242223/files")
+        const datas = result.data;
+        for (data of datas) {
+            switch (data.releaseType) {
+                case 1:
+                    data.releaseTypeStr = 'Release';
+                    break;
+                case 2:
+                    data.releaseTypeStr = 'Beta';
+                    break;
+                case 3:
+                    data.releaseTypeStr = 'Alpha';
+                    break;
+                default:
+                    data.releaseTypeStr = `Unknow: ${data.releaseType}`;
+                    break;
+            }
+            if (Array.isArray(data.gameVersion)) {
+                data.gameVersion = data.gameVersion.join(', ');
+            }
         }
-        if (Array.isArray(data.gameVersion)) {
-            data.gameVersion = data.gameVersion.join(', ');
-        }
+        return datas;
+    } catch (e) {
+        helpers.log.error(e);
+        return [];
     }
-    return datas;
 }
